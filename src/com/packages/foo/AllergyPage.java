77134df0ab67b1023e35874240bb624fb11ba248
kdaud <dkakumirizii@gmail.com>
2021-02-09 11:30:23 +0300

    More api docs added

--- src/com/packages/foo/AllergyPage.java
+++ src/com/packages/foo/AllergyPage.java
@@ -3,13 +3,22 @@ package com.packages.foo;
 
 public class AllergyPage {
 	
+	static {
+		System.out.println("God is good. All the time");
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		gradCompute("GPA");
 	}
 	
 	static {
 		System.out.println("Good Day");
+	}
+	
+	public static void gradCompute(String studentName) {
+		double res = 519.5 + 75 + 30;
+		double myRes = 107 + 15 + 6;
+		System.out.println(myRes);
+		System.out.println(studentName + ": " + res / myRes);
 	}
 	
 }
