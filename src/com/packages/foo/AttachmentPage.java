4a6787a00125c2df0b6700946958911f2d84159b
kdaud <dkakumirizii@gmail.com>
2021-02-24 15:01:58 +0300

    Static field should be accessed at class level

---
+++ src/com/packages/foo/AttachmentPage.java
@@ -0,0 +1,12 @@
+package com.packages.foo;
+
+
+public class AttachmentPage {
+	
+	public static final String UNIV_NAME = "Canada University of Technology";
+	
+	public static String myFellowshipJourney(String department) {
+		return "Quality Assurance " + department;
+	}
+	
+}
---
+++ src/com/packages/foo/TODO.java
@@ -0,0 +1,13 @@
+package com.packages.foo;
+
+
+public class TODO {
+	
+	private AttachmentPage attachmentPage;
+	public static void main(String[] args) {
+		
+		System.out.println(AttachmentPage.UNIV_NAME);
+		System.out.println(AttachmentPage.myFellowshipJourney("Engineer"));
+	}
+	
+}
